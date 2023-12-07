1855ea285d7b88117b91c7d1b6d5b819eb271af5
Mike Bland <mbland@acm.org>
2023-12-07 11:03:59 -0500

    Update test to fail on unset config.base
    `pnpm test:workaround`, which deletes config.base, will now timeout.
    
    This is in response to:
    
    - https://github.com/vitest-dev/vitest/issues/4686#issuecomment-1844852092

--- demo.test.js
+++ demo.test.js
@@ -1,3 +1,11 @@
-import { test } from 'vitest'
+import { expect, test } from 'vitest'
 
-test('OK in <=1.0.0-beta.4+vite <=5.0.0-beta.7, breaks otherwise', () => null)
+test('OK in <=1.0.0-beta.4+vite <=5.0.0-beta.7, breaks otherwise', async () => {
+  // This will timeout when config.base is undefined.
+  var page = window.open('/foobar/hello.html')
+  await new Promise(resolve => page.addEventListener('load', resolve))
+
+  let e = page.document.querySelector('body p')
+
+  expect(e.textContent).toContain('Hello, World!')
+})
